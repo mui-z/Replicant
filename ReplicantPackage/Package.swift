30ff95771b76b1b26631ed35514f5fc6142c5163
mui-z <93278577+mui-z@users.noreply.github.com>
2024-02-12 03:10:43 +0900

    fixed package ref

--- ReplicantPackage/Package.swift
+++ ReplicantPackage/Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.7.3"),
+    .package(url: "git@github.com:petitstrawberry/MisskeyAPIKit.git", from: "0.2.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,6 +27,10 @@ let package = Package(
         .product(
           name: "ComposableArchitecture",
           package: "swift-composable-architecture"
+        ),
+        .product(
+          name: "MisskeyAPIKit",
+          package: "MisskeyAPIKit"
         )
       ]),
     .testTarget(
