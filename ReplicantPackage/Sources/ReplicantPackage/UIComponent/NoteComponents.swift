0d9d684e204e19ff0ec630a391390d1c8e9713fe
mui-z <93278577+mui-z@users.noreply.github.com>
2024-02-24 12:10:37 +0900

    refactor

--- ReplicantPackage/Sources/ReplicantPackage/UIComponent/NoteComponents.swift
+++ ReplicantPackage/Sources/ReplicantPackage/UIComponent/NoteComponents.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct Icon: View {
+  let size = 40.0
   var body: some View {
     Circle()
-      .frame(width: 50, height: 50)
+      .frame(width: size, height: size)
       .foregroundStyle(.cyan)
   }
 }
 
 struct NameBar: View {
   var body: some View {
-    HStack(spacing: 5) {
+    HStack(spacing: 0) {
       Text("name")
       Text("@id")
         .foregroundStyle(.gray)
@@ -33,27 +34,39 @@ struct NoteText: View {
   }
 }
 
-
 struct NoteReactionBar: View {
+  let width = 40.0
   var body: some View {
     HStack(spacing: 20) {
-      HStack(spacing: 2) {
-        Image(systemName: "arrow.uturn.left")
-          .imageScale(.small)
-        Text("12")
-          .font(.caption)
-      }
-      HStack(spacing: 2) {
-        Image(systemName: "arrow.2.squarepath")
-          .imageScale(.small)
-        Text("12")
-          .font(.caption)
-      }
-      Image(systemName: "plus")
-        .imageScale(.small)
-      Image(systemName: "ellipsis")
-        .imageScale(.small)
+      withCountButton(systemName: "arrow.uturn.left", count: 12)
+      withCountButton(systemName: "arrow.2.squarepath", count: 12)
+      button(systemName: "plus")
+      button(systemName: "ellipsis")
     }
     .foregroundStyle(.gray)
+  }
+
+  private func withCountButton(systemName: String, count: Int) -> some View {
+    Button {
+      print("pushed")
+    } label: {
+      HStack(spacing: 2) {
+        Image(systemName: systemName)
+          .imageScale(.small)
+        Text(count.description)
+          .font(.caption)
+      }
+      .frame(width: width, alignment: .leading)
+    }
+  }
+
+  private func button(systemName: String) -> some View {
+    Button {
+      print("normal button")
+    } label: {
+      Image(systemName: systemName)
+        .imageScale(.small)
+        .frame(width: width, alignment: .leading)
+    }
   }
 }
